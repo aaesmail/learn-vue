ae544c55bd25053c7442547f35637ff6aff28a02
Ali Adel <42678489+lFirebolt@users.noreply.github.com>
2021-05-02 01:25:13 +0200

    added vuex

---
+++ vuex/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <base-container v-if="authenticated" title="Vuex">
+    <the-counter></the-counter>
+    <favorite-value></favorite-value>
+    <button @click="addOne">Add 10</button>
+    <change-counter></change-counter>
+  </base-container>
+  <base-container title="Auth">
+    <user-auth></user-auth>
+  </base-container>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter';
+import ChangeCounter from './components/ChangeCounter';
+import FavoriteValue from './components/FavoriteValue';
+import UserAuth from './components/UserAuth';
+
+export default {
+  components: {
+    BaseContainer,
+    TheCounter,
+    ChangeCounter,
+    FavoriteValue,
+    UserAuth,
+  },
+
+  computed: {
+    ...mapGetters(['authenticated']),
+  },
+
+  methods: {
+    addOne() {
+      // this.$store.commit('increase', { value: 10 });
+      // this.$store.commit({
+      //   type: 'increase',
+      //   value: 10,
+      // });
+      this.$store.dispatch({
+        type: 'numbers/increase',
+        value: 10,
+      });
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ vuex/src/components/UserAuth.vue
@@ -0,0 +1,17 @@
+<template>
+  <button v-if="authenticated" @click="logout">Logout</button>
+  <button v-else @click="login">Login</button>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+  computed: {
+    ...mapGetters(['authenticated']),
+  },
+  methods: {
+    ...mapActions(['login', 'logout']),
+  },
+};
+</script>
